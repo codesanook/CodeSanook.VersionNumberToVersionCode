66c9b66ff77cfbdbae2e435f7118c96372ce04fc
aaronamm <theeranitp@gmail.com>
2018-11-11 08:46:25 +0700

    remove nuget exe and add appveyor.yml

--- Tools/Add-Nuspec.ps1
+++
@@ -1,7 +0,0 @@
-#Create nuspec file if not existing
-#more info https://docs.microsoft.com/en-us/nuget/tools/cli-ref-spec
-# nuspec reference https://docs.microsoft.com/en-us/nuget/reference/nuspec
-Push-Location
-Set-Location ..\CodeSanook.VersionNumberToVersionCode
-..\Tools\nuget.exe spec
-Pop-Location
